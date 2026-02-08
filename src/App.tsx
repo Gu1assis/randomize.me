02195a6b15b7fb05790c5def99afb3b708a79a29
Gu1Assis <assis.guisilva@gmail.com>
2026-02-08 20:01:07 -0300

    🎉 hello world

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import logo from './assets/logo.jpeg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/Gu1assis/randomize.me" target="_blank">
+          <img src={logo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>This is a Work in Progress</h1>
+      <p className="read-the-docs">
+        Uma coisa de cada vez
+      </p>
+    </>
+  )
+}
+
+export default App
